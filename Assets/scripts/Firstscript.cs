ef0a27caa9ab08fc4e0243964edc5e92d3800b1b
wththd <kolyagonza@gmail.com>
2024-05-27 21:05:01 +0300

    push changed files

--- Assets/scripts/Firstscript.cs
+++ Assets/scripts/Firstscript.cs
@@ -1,20 +1,49 @@
-using System;
 using UnityEngine;
+using System;
 
-public class Firstscript : MonoBehaviour {
-    
+namespace Test
+{
+    public class Firstscript : MonoBehaviour
+    {
+        /*
+         //bool true - false
+         //double 
+         //decimal 11.1
+         //short long
+        */
+        public float speed;
 
-    public float speed;
-    
-    // Start is called before the first frame update
-    void Start() {
-        
+        // Start is called before the first frame update
+        public void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            int newSpeed = 5;
+            print(" Car move " + newSpeed);
+            Debug.Log(" Car move" + System.Math.PI);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+            
+        }
+
+        Vector2 GetPosition()
+        {
+            return transform.position;
+        }
+
+        int GetResult(string text)
+        {
+            // Parsing 
+            return 2 + 2; 
+        }
+
+        bool IsInSight()
+        {
+            return true;
+        }
+
     }
-
-    // Update is called once per frame
-    void Update() {
-        print ( " Car move " );
-        transform.Translate (speed, 0,0 );
-    }
-    
 }
